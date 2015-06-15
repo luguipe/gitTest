3add5483e1669dd83c0f90dbe0392f524727ffec
2104689414 <2104689414@CCI-12961.tafe.mst>
2015-06-15 10:27:43 +1000

    new commit

---
+++ src/FindMyCareer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package findmycareer;
+
+/**
+ *
+ * @author 2104689414
+ */
+public class FindMyCareer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
